5a42e660b0c4508d0f4153a87e77f71ab599839c
Hitman007 <support@customrayguns.com>
2017-02-27 15:00:05 -0800

    Update wordpress.sh

--- installScripts/wordpress.sh
+++ installScripts/wordpress.sh
@@ -31,3 +31,20 @@ sudo chmod -R g+w /var/www/html/wp-content/plugins
 sudo chmod -R g+w /var/www/html/wp-content/themes
 sudo rm -fr akismet
 sudo rm hello.php
+cd /var/www/wp-content/plugins
+sudo chown -R ubuntu:www-data /var/www/html
+sudo find /var/www/html -type d -exec chmod g+s {} \;
+sudo chmod g+w /var/www/html/wp-content
+sudo chmod -R g+w /var/www/html/wp-content/plugins
+sudo chmod -R g+w /var/www/html/wp-content/themes
+sudo rm -fr akismet
+sudo rm hello.php
+sudo git clone https://github.com/Hitman007/Wordpress-Pickles.git
+sudo git clone https://Hitman007@bitbucket.org/Hitman007/crg_mods.git
+mysql -u root -ppassword << EOF
+CREATE DATABASE wordpress DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci;
+CREATE USER 'wordpressuser'@'localhost' IDENTIFIED BY 'password';
+GRANT ALL ON wordpress.* TO 'wordpressuser'@'localhost' IDENTIFIED BY 'password';
+FLUSH PRIVILEGES;
+EOF
+sudo reboot
