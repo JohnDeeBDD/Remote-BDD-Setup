64a03409f5200dfc4052e1403291725f34d1bb38
Hitman007 <support@customrayguns.com>
2017-02-27 14:51:49 -0800

    Create wordpress.sh

---
+++ installScripts/wordpress.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+sudo apt-get -y update
+sudo apt-get -y upgrade
+sudo chmod -R 777 /var/www
+sudo apt-get -f install
+sudo apt-get -y install nodejs-legacy
+sudo apt-get -y install php
+sudo apt-get -y install php-curl
+sudo apt-get -y install php-gd
+sudo apt-get -y install php-mbstring
+sudo apt-get -y install php-mcrypt
+sudo apt-get -y install php-xml
+sudo apt-get -y install php-xmlrpc
+sudo apt-get -y install default-jdk
+sudo apt-get -y install libxss1
+sudo apt-get -y install libappindicator1
+sudo apt-get -y install libindicator7
+sudo apt-get -y install npm
+sudo apt-get -y install zip
+sudo apt-get -y install unzip
+sudo apt-get -y install php-zip
+sudo apt-get -y install lamp-server^
+curl -sS https://getcomposer.org/installer | sudo php -- --install-dir=/usr/local/bin --filename=composer
+sudo a2enmod rewrite
