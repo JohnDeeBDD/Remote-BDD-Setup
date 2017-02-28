609472a38ec11decd9bfe083557733f748300f06
Hitman007 <support@customrayguns.com>
2017-02-27 17:18:07 -0800

    Update wordpress.sh

--- installScripts/wordpress.sh
+++ installScripts/wordpress.sh
@@ -23,6 +23,11 @@ sudo apt-get -y install lamp-server^
 curl -sS https://getcomposer.org/installer | sudo php -- --install-dir=/usr/local/bin --filename=composer
 sudo a2enmod rewrite
 sudo chmod -R 777 /var/www
+cd /var/www/html
+wget http://wordpress.org/latest.tar.gz
+tar xfz latest.tar.gz
+mv -v /var/www/html/wordpress/* /var/www/html/
+rm -fr /var/www/html/wordpress
 sudo chown -R ubuntu:www-data /var/www/html
 sudo find /var/www/html -type d -exec chmod g+s {} \;
 sudo chmod g+w /var/www/html/wp-content
@@ -35,7 +40,6 @@ sudo chmod -R g+w /var/www/html/wp-content/plugins
 sudo chmod -R g+w /var/www/html/wp-content/themes
 cd /var/www/html/wp-content/plugins
 sudo rm -fr akismet
-sudo rm hello.php
 sudo rm hello.php
 sudo git clone https://github.com/Hitman007/Wordpress-Pickles.git
 sudo git clone https://Hitman007@bitbucket.org/Hitman007/crg_mods.git
