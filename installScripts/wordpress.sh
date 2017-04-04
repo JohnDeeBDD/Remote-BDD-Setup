d577f863ec82a4ab21d6119316d2d4f3851844e4
Hitman007 <support@customrayguns.com>
2017-04-03 20:19:01 -0700

    Update wordpress.sh

--- installScripts/wordpress.sh
+++ installScripts/wordpress.sh
@@ -2,6 +2,42 @@
 #
 # source <(curl -s https://raw.githubusercontent.com/Hitman007/Remote-BDD-Setup/master/installScripts/wordpress.sh)
 #
+sudo apt-get -y update
+sudo apt-get -y upgrade
+sudo chmod -R 777 /var/www
+sudo wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+sudo dpkg -i google-chrome*.deb
+sudo apt-get -f install
+sudo apt-get -y install nodejs-legacy
+sudo apt-get -y install xfce4
+sudo apt-get -y install vnc4server
+sudo apt-get -y install php
+sudo apt-get -y install php-curl
+sudo apt-get -y install php-gd
+sudo apt-get -y install php-mbstring
+sudo apt-get -y install php-mcrypt
+sudo apt-get -y install php-xml
+sudo apt-get -y install php-xmlrpc
+sudo apt-get -y install firefox
+sudo apt-get -y install default-jdk
+sudo apt-get -y install libxss1
+sudo apt-get -y install libappindicator1
+sudo apt-get -y install libindicator7
+sudo apt-get -y install npm
+sudo apt-get -y install zip
+sudo apt-get -y install unzip
+sudo apt-get -y install php-zip
+sudo apt-get -y install lamp-server^
+curl -sS https://getcomposer.org/installer | sudo php -- --install-dir=/usr/local/bin --filename=composer
+sudo a2enmod rewrite
+
+# Install ChromeDriver.
+wget -N http://chromedriver.storage.googleapis.com/2.27/chromedriver_linux64.zip -P ~/
+unzip ~/chromedriver_linux64.zip -d ~/
+rm ~/chromedriver_linux64.zip
+sudo mv -f ~/chromedriver /usr/local/share/
+sudo chmod +x /usr/local/share/chromedriver
+sudo ln -s /usr/local/share/chromedriver /usr/local/bin/chromedriver
 
 sudo chmod -R 777 /var/www
 cd /var/www/html
