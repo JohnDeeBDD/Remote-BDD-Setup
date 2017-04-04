cf6c5e8154829120266e448c9697670d959cabb8
Hitman007 <support@customrayguns.com>
2017-04-03 19:38:04 -0700

    Update init.sh

--- installScripts/init.sh
+++ installScripts/init.sh
@@ -27,3 +27,11 @@ sudo apt-get -y install php-zip
 sudo apt-get -y install lamp-server^
 curl -sS https://getcomposer.org/installer | sudo php -- --install-dir=/usr/local/bin --filename=composer
 sudo a2enmod rewrite
+
+# Install ChromeDriver.
+wget -N http://chromedriver.storage.googleapis.com/2.27/chromedriver_linux64.zip -P ~/
+unzip ~/chromedriver_linux64.zip -d ~/
+rm ~/chromedriver_linux64.zip
+sudo mv -f ~/chromedriver /usr/local/share/
+sudo chmod +x /usr/local/share/chromedriver
+sudo ln -s /usr/local/share/chromedriver /usr/local/bin/chromedriver
