d9245e12d4a5d563a16cf089b54c64140b349049
Hitman007 <support@customrayguns.com>
2017-04-03 19:43:19 -0700

    Update init2.sh

--- installScripts/init2.sh
+++ installScripts/init2.sh
@@ -1,12 +1,5 @@
 #!/bin/sh
-cd /var
-#sudo chmod -R 777 /var/www
-#sudo rm -fr /var/www
-#sudo git clone https://github.com/Hitman007/Remote-BDD-Setup.git
-#sudo mv /var/Remote-BDD-Setup /var/www
-#sudo chmod -R 777 /var/www
-sudo mv /var/www/installScripts/apache2.conf /etc/apache2/apache2.conf
-sudo mv /var/www/installScripts/xstartup ~/.vnc/xstartup
+
 cd /var/www/html/wp-content/plugins
 sudo chown -R ubuntu:www-data /var/www/html
 sudo find /var/www/html -type d -exec chmod g+s {} \;
@@ -17,4 +10,3 @@ sudo rm -fr akismet
 sudo rm hello.php
 sudo git clone https://github.com/Hitman007/Wordpress-Pickles.git
 sudo git clone https://Hitman007@bitbucket.org/Hitman007/crg_mods.git
-sudo reboot
