fd1d1a32570411f78a689e0c443f957aa83b1447
Hitman007 <support@customrayguns.com>
2017-04-13 15:05:39 -0700

    Update wordpress.sh

--- installScripts/wordpress.sh
+++ installScripts/wordpress.sh
@@ -7,24 +7,15 @@ sudo apt-get -y update
 sudo apt-get -y upgrade
 sudo chmod -R 777 /var/www
 sudo wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-# sudo dpkg -i google-chrome*.deb
 sudo dpkg -y -i google-chrome*.deb
 sudo apt-get -f install
 sudo apt-get -y install nodejs-legacy
 sudo apt-get -y install xfce4
 sudo apt-get -y install vnc4server
-sudo apt-get -y install php
-sudo apt-get -y install php-curl
-sudo apt-get -y install php-gd
-sudo apt-get -y install php-mbstring
-sudo apt-get -y install php-mcrypt
-sudo apt-get -y install php-xml
-sudo apt-get -y install php-xmlrpc
+sudo apt-get -y install php php-curl php-gd php-mbstring php-mcrypt php-xml php-xmlrpc
 sudo apt-get -y install firefox
 sudo apt-get -y install default-jdk
-sudo apt-get -y install libxss1
-sudo apt-get -y install libappindicator1
-sudo apt-get -y install libindicator7
+sudo apt-get -y install libxss1 libappindicator1 libindicator7
 sudo apt-get -y install npm
 sudo apt-get -y install zip
 sudo apt-get -y install unzip
@@ -32,9 +23,10 @@ sudo apt-get -y install php-zip
 sudo debconf-set-selections <<< 'mysql-server mysql-server/root_password password password'
 sudo debconf-set-selections <<< 'mysql-server mysql-server/root_password_again password password'
 sudo apt-get -y install lamp-server^
-curl -sS https://getcomposer.org/installer | sudo php -- --install-dir=/usr/local/bin --filename=composer
-
 sudo a2enmod rewrite
+
+# Composer:
+curl -sS https://getcomposer.org/installer | sudo php -- --install-dir=/usr/local/bin --filename=composer
 
 # Install Wordpress:
 sudo chmod -R 777 /var/www
