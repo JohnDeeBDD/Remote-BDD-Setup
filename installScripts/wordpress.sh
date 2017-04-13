b3ff7a1681230af621b3b3355596099ca74e026f
Hitman007 <support@customrayguns.com>
2017-04-13 14:19:37 -0700

    Update wordpress.sh

--- installScripts/wordpress.sh
+++ installScripts/wordpress.sh
@@ -77,9 +77,9 @@ chmod +x wp-cli.phar
 sudo mv wp-cli.phar /usr/local/bin/wp
 
 #install phantomJS
-sudo apt-get install build-essential chrpath libssl-dev libxft-dev
-sudo apt-get install libfreetype6 libfreetype6-dev
-sudo apt-get install libfontconfig1 libfontconfig1-dev
+sudo apt-get -y install build-essential chrpath libssl-dev libxft-dev
+sudo apt-get -y install libfreetype6 libfreetype6-dev
+sudo apt-get -y install libfontconfig1 libfontconfig1-dev
 #to run phantomjs:
 # ./phantomjs --webdriver=4444
 
