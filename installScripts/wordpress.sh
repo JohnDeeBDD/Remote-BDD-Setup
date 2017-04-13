aaf75ccfe1426da5bcdfb3d8804c31c717dc298e
Hitman007 <support@customrayguns.com>
2017-04-13 14:39:48 -0700

    Update wordpress.sh

--- installScripts/wordpress.sh
+++ installScripts/wordpress.sh
@@ -9,6 +9,9 @@ sudo chmod -R 777 /var/www
 sudo wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 sudo dpkg -i google-chrome*.deb
 sudo apt-get -f install
+sudo debconf-set-selections <<< 'mysql-server mysql-server/root_password password password'
+sudo debconf-set-selections <<< 'mysql-server mysql-server/root_password_again password password'
+sudo apt-get -y install mysql-server
 sudo apt-get -y install nodejs-legacy
 sudo apt-get -y install xfce4
 sudo apt-get -y install vnc4server
@@ -28,9 +31,8 @@ sudo apt-get -y install npm
 sudo apt-get -y install zip
 sudo apt-get -y install unzip
 sudo apt-get -y install php-zip
-sudo debconf-set-selections <<< 'mysql-server mysql-server/root_password password password'
-sudo debconf-set-selections <<< 'mysql-server mysql-server/root_password_again password password'
-sudo apt-get -y install mysql-server
+
+#sudo apt-get -y install lamp-server^
 curl -sS https://getcomposer.org/installer | sudo php -- --install-dir=/usr/local/bin --filename=composer
 
 sudo a2enmod rewrite
