d53bc4cc13742dc99df5e21ff7d49c32f4017868
Hitman007 <support@customrayguns.com>
2017-04-24 00:35:36 -0700

    Update wordpress.sh

--- installScripts/wordpress.sh
+++ installScripts/wordpress.sh
@@ -68,7 +68,6 @@ cd /var/www/html/wp-content/plugins
 sudo rm -fr akismet
 sudo rm hello.php
 sudo git clone https://github.com/Hitman007/Wordpress-Pickles.git
-sudo git clone https://Hitman007@bitbucket.org/Hitman007/crg_mods.git
 sudo git clone https://github.com/Hitman007/NevadaCat.git
 sudo git clone https://github.com/Hitman007/CRG-Mods.git
 sudo wp plugin install woocommerce
