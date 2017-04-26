c6ac9caeb89423b1f9d1f1e65e77d109cbf8d932
Hitman007 <support@customrayguns.com>
2017-04-25 21:17:33 -0700

    Create ResetWPdatabases.sh

---
+++ installScripts/ResetWPdatabases.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+#
+# source <(curl -s https://raw.githubusercontent.com/Hitman007/Remote-BDD-Setup/master/installScripts/wordpress.sh)
+
+mysql -u root -ppassword << EOF
+DROP DATABASE wordpress;
+CREATE DATABASE wordpress DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci;
+CREATE DATABASE wordprees_unit_test DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci;
+GRANT ALL ON wordpress.* TO 'wordpressuser'@'localhost' IDENTIFIED BY 'password';
+GRANT ALL ON wordprees_unit_test.* TO 'wordpressuser'@'localhost' IDENTIFIED BY 'password';
+FLUSH PRIVILEGES;
+EOF
