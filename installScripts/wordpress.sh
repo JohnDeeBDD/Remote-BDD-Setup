4293ca12ce2c0b48f8f9819ec0aa02cc90a9bddf
Hitman007 <support@customrayguns.com>
2017-05-14 09:22:20 -0700

    Update wordpress.sh

--- installScripts/wordpress.sh
+++ installScripts/wordpress.sh
@@ -68,10 +68,10 @@ sudo rm -fr akismet
 sudo rm hello.php
 sudo git clone https://github.com/Hitman007/Wordpress-Pickles.git
 sudo git clone https://github.com/Hitman007/CRG-Mods.git
-https://github.com/Hitman007/WPBDD-Scaffold.git
-https://github.com/Hitman007/WPBDD-First-failing-test.git
-https://github.com/Hitman007/WPBDD-First-passing-test.git
-https://github.com/Hitman007/WPBDDTUT-FINISH.git
+sudo git clone https://github.com/Hitman007/WPBDD-Scaffold.git
+sudo git clone https://github.com/Hitman007/WPBDD-First-failing-test.git
+sudo git clone https://github.com/Hitman007/WPBDD-First-passing-test.git
+sudo git clone https://github.com/Hitman007/WPBDDTUT-FINISH.git
 
 sudo chmod -R 777 /var/www
 
