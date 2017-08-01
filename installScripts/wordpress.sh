491bc0ab045098a2c3060903f9fa1ae169a24230
Hitman007 <support@customrayguns.com>
2017-08-01 10:32:14 -0700

    Update wordpress.sh

--- installScripts/wordpress.sh
+++ installScripts/wordpress.sh
@@ -5,7 +5,6 @@
 #
 sudo apt-get -y update
 sudo apt-get -y upgrade
-sudo chmod -R 777 /var/www
 sudo wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 sudo dpkg -i google-chrome*.deb
 sudo apt-get -f install
