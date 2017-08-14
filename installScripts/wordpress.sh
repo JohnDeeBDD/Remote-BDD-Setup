03218d8195a0443636e075378083b6379e331aae
Hitman007 <support@customrayguns.com>
2017-08-13 19:20:19 -0700

    Update wordpress.sh

--- installScripts/wordpress.sh
+++ installScripts/wordpress.sh
@@ -7,7 +7,7 @@ sudo apt-get -y update
 sudo apt-get -y upgrade
 sudo wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 sudo dpkg -i google-chrome*.deb
-#sudo apt-get -f install
+sudo apt-get -y -f install
 sudo apt-get -y install nodejs-legacy
 sudo apt-get -y install ubuntu-desktop gnome-panel gnome-settings-daemon metacity nautilus gnome-terminal
 sudo apt-get -y install vnc4server
