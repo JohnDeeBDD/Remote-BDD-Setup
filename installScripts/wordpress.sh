e0c5cc85954d4ffcad162661fafc4a487de40fdb
Hitman007 <support@customrayguns.com>
2017-08-13 18:34:17 -0700

    Update wordpress.sh

--- installScripts/wordpress.sh
+++ installScripts/wordpress.sh
@@ -78,21 +78,20 @@ sudo apt-get -y install libfontconfig1 libfontconfig1-dev
 #to run phantomjs:
 # ./phantomjs --webdriver=4444
 
-#set VNC password
-#prog=/usr/bin/vncpasswd
-#mypass="password"
+prog=/usr/bin/vncpasswd
+mypass="newpass"
 
-#/usr/bin/expect <<EOF
-#spawn "$prog"
-#expect "Password:"
-#send "$mypass\r"
-#expect "Verify:"
-#send "$mypass\r"
-#expect eof
-#exit
-#EOF
-#sudo vncserver :1
-#sudo vncserver -kill :1
+/usr/bin/expect <<EOF
+spawn "$prog"
+expect "Password:"
+send "$mypass\r"
+expect "Verify:"
+send "$mypass\r"
+expect eof
+exit
+EOF
+sudo vncserver :1
+vncserver -kill :1
 
 sudo apt-get clean
 sudo reboot
