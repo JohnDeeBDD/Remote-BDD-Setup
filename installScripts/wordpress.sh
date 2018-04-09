da0cf2b5632d70803ac1bfbbc2ed3d5b84fda27f
John Dee <33011694+JohnDeeBDD@users.noreply.github.com>
2018-04-09 16:42:42 -0700

    Update wordpress.sh

--- installScripts/wordpress.sh
+++ installScripts/wordpress.sh
@@ -4,10 +4,13 @@
 # source <(curl -s https://raw.githubusercontent.com/johndeebdd/Remote-BDD-Setup/master/installScripts/wordpress.sh)
 # Directions: https://wp-bdd.com/bdd-wp-aws/
 #
+#sudo wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+#sudo dpkg -i google-chrome*.deb
+wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
+echo 'deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main' | sudo tee /etc/apt/sources.list.d/google-chrome.list
 sudo apt-get -y update
-sudo apt-get -y upgrade
-sudo wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-sudo dpkg -i google-chrome*.deb
+sudo apt-get -y upgrade 
+sudo apt-get install google-chrome-stable
 sudo apt-get -f install
 sudo apt-get -y install nodejs-legacy
 sudo apt-get -y install ubuntu-desktop gnome-panel gnome-settings-daemon metacity nautilus gnome-terminal
