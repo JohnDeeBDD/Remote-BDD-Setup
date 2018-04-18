0a94fd8446679579192c3f8e5528424358e69413
John Dee <33011694+JohnDeeBDD@users.noreply.github.com>
2018-04-18 14:16:19 -0700

    Update addUbuntuAdmin.sh

--- installScripts/addUbuntuAdmin.sh
+++ installScripts/addUbuntuAdmin.sh
@@ -7,7 +7,7 @@ echo Add a user?
 read USR
 echo Password?
 read PASS
-sudo adduser $USR --gecos "First Last,RoomNumber,WorkPhone,HomePhone" --disabled-password
+sudo adduser $USR --gecos "First Last,RoomNumber,WorkPhone,HomePhone" -p$PASS
 # This will actually set the password:
 echo "$USR:$PASS" | chpasswd
 sudo usermod -aG sudo $USR
