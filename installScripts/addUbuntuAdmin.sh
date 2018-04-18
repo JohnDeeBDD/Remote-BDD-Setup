a1960ddd272c55388a937d0c6701dd230a509c4c
John Dee <33011694+JohnDeeBDD@users.noreply.github.com>
2018-04-18 13:48:51 -0700

    Update addUbuntuAdmin.sh

--- installScripts/addUbuntuAdmin.sh
+++ installScripts/addUbuntuAdmin.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 #
 # you may have to install curl first with: sudo apt-get install curl
-# source <(curl -s https://raw.githubusercontent.com/johndeebdd/Remote-BDD-Setup/master/installScripts/rep.sh)
+# source <(curl -s https://raw.githubusercontent.com/johndeebdd/Remote-BDD-Setup/master/installScripts/addUbuntuAdmin.sh)
 echo Add a user?
 read USR
 echo Password?
