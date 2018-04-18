c72b28ebe26ac518b84827e0ae121f5e7200db61
John Dee <33011694+JohnDeeBDD@users.noreply.github.com>
2018-04-18 13:48:27 -0700

    Update and rename rep.sh to addUbuntuAdmin.sh

--- installScripts/addUbuntuAdmin.sh
+++ installScripts/addUbuntuAdmin.sh
@@ -2,7 +2,12 @@
 #
 # you may have to install curl first with: sudo apt-get install curl
 # source <(curl -s https://raw.githubusercontent.com/johndeebdd/Remote-BDD-Setup/master/installScripts/rep.sh)
-echo What is the URL of the site?
-read varurl
-cd /var/www/html/wp-content/plugins/WPbdd/tests
-replace "http://replaceme.com" $varurl -- runner.suite.yml
+echo Add a user?
+read USR
+echo Password?
+read PASS
+adduser $USR
+# This will actually set the password:
+echo "$USR:$PASS" | chpasswd
+adduser $USR
+usermod -aG sudo $USR
