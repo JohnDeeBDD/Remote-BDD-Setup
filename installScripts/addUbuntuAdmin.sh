fa221293e34525f82da8489a5025a7678dc9fceb
John Dee <33011694+JohnDeeBDD@users.noreply.github.com>
2018-04-19 23:21:48 -0700

    Update addUbuntuAdmin.sh

--- installScripts/addUbuntuAdmin.sh
+++ installScripts/addUbuntuAdmin.sh
@@ -3,11 +3,9 @@
 # you may have to install curl first with: sudo apt-get install curl
 # source <(curl -s https://raw.githubusercontent.com/johndeebdd/Remote-BDD-Setup/master/installScripts/addUbuntuAdmin.sh)
 clear
-echo Add a user?
-read USR
-echo Password?
-read PASS
-sudo adduser $USR --gecos "First Last,RoomNumber,WorkPhone,HomePhone" --disabled-password
+#removes password auth
+sudo sed -i -e '/^PasswordAuthentication / s/ .*/ yes/' /etc/ssh/sshd_config
+sudo adduser freelancer --gecos "First Last,RoomNumber,WorkPhone,HomePhone" --disabled-password
 # This will actually set the password:
-echo "$USR:$PASS" | sudo chpasswd
-sudo usermod -aG sudo $USR
+echo "freelancer:password" | sudo chpasswd
+sudo usermod -aG sudo freelancer
