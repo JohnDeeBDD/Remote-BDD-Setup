abcd244b035f1a87ec6694d561a33ee68caa1604
John Dee <33011694+JohnDeeBDD@users.noreply.github.com>
2018-08-16 23:01:26 -0700

    Create installplugin.sh

---
+++ installScripts/installplugin.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+#
+# you may have to install curl first with: sudo apt-get install curl
+# source <(curl -s https://raw.githubusercontent.com/johndeebdd/Remote-BDD-Setup/master/installScripts/installplugin.sh)
+#
+clear
+echo What is the domain name of the site?
+read varurl
+clear
+echo What is the git clone URL?
+read varurl
+clear
+echo What is the directory name?
+read varurl
+cd /var/www/html/wp-content/plugins
+git clone $vargit
+cd $vardir
+composer install
+cd tests/acceptance
+sudo replace "replaceme.com" $varurl -- acceptance.suite.yml
