fa00535ba1ba939eea25a9571f8d24df0248b3c7
John Dee <33011694+JohnDeeBDD@users.noreply.github.com>
2018-11-17 17:25:40 -0800

    Update ResetWPdatabases.sh

--- installScripts/ResetWPdatabases.sh
+++ installScripts/ResetWPdatabases.sh
@@ -2,7 +2,7 @@
 #
 # source <(curl -s https://raw.githubusercontent.com/JohnDeeBDD/Remote-BDD-Setup/master/installScripts/ResetWPdatabases.sh)
 
-sudo mysql -u root << EOF
+sudo mysql -u root -ppassword << EOF
 DROP DATABASE wordpress;
 CREATE DATABASE wordpress DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci;
 GRANT ALL ON wordpress.* TO 'wordpressuser'@'localhost' IDENTIFIED BY 'password';
