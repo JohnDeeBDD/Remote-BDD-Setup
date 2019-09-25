df4fce679742ac557dd2df372c2abdee761be900
John Dee <33011694+JohnDeeBDD@users.noreply.github.com>
2019-09-25 13:35:39 -0700

    Update wordpress.sh

--- installScripts/wordpress.sh
+++ installScripts/wordpress.sh
@@ -9,12 +9,12 @@
 clear
 echo What is the domain name of the site?
 read varurl
-wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
-echo 'deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main' | sudo tee /etc/apt/sources.list.d/google-chrome.list
+#wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
+#echo 'deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main' | sudo tee /etc/apt/sources.list.d/google-chrome.list
 sudo apt-get -y update
 sudo apt-get -y upgrade 
 #sudo apt-get install google-chrome-stable
-sudo apt-get -y install google-chrome-stable
+#sudo apt-get -y install google-chrome-stable
 #sudo apt-get -f install
 #sudo apt-get -y install nodejs-legacy
 sudo apt-get -y install ubuntu-desktop gnome-panel gnome-settings-daemon metacity nautilus gnome-terminal
