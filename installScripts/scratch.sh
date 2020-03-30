0bc0f5eec5625f52ef8b717f4dec4a832933cca0
John Dee <33011694+JohnDeeBDD@users.noreply.github.com>
2020-03-30 03:17:42 -0700

    Update scratch.sh

--- installScripts/scratch.sh
+++ installScripts/scratch.sh
@@ -1,2 +1,3 @@
 #source <(curl -s https://raw.githubusercontent.com/johndeebdd/Remote-BDD-Setup/master/installScripts/scratch.sh)
 clear
+echo 'define('FS_METHOD', "direct");' >> /var/www/html/wp-config.php
