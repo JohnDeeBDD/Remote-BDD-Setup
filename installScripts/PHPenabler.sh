6a3050726f2af35d673f4898c982039fbb41ae5e
John Dee <33011694+JohnDeeBDD@users.noreply.github.com>
2020-07-25 11:33:24 -0700

    Create PHPenabler.sh

---
+++ installScripts/PHPenabler.sh
@@ -0,0 +1,35 @@
+#- /etc/php/7.2/cli/conf.d/10-mysqlnd.ini
+#  - /etc/php/7.2/cli/conf.d/10-opcache.ini
+#  - /etc/php/7.2/cli/conf.d/10-pdo.ini
+#  - /etc/php/7.2/cli/conf.d/15-xml.ini
+#  - /etc/php/7.2/cli/conf.d/20-calendar.ini
+# source <(curl -s https://raw.githubusercontent.com/JohnDeeBDD/Remote-BDD-Setup/master/installScripts/PHPenabler.sh)
+sudo phpenmod
+sudo phpenmod
+sudo phpenmod ctype
+sudo phpenmod curl
+sudo phpenmod exif
+sudo phpenmod fileinfo
+sudo phpenmod ftp
+sudo phpenmod gd
+sudo phpenmod gettext
+sudo phpenmod iconv 
+sudo phpenmod json
+sudo phpenmod mysqli
+sudo phpenmod pdo_mysql
+sudo phpenmod phar
+sudo phpenmod posix
+sudo phpenmod readline
+sudo phpenmod shmop
+sudo phpenmod simplexml
+sudo phpenmod sockets
+sudo phpenmod sysvmsg
+sudo phpenmod sysvsem
+sudo phpenmod sysvshm
+sudo phpenmod tokenizer
+sudo phpenmod wddx
+sudo phpenmod xmlreader
+sudo phpenmod xmlrpc
+sudo phpenmod xmlwriter
+sudo phpenmod xsl
+sudo phpenmod zip
