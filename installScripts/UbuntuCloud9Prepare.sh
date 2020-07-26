d3de1c4c38a6312b2849e5caff42473a29d82da8
John Dee <33011694+JohnDeeBDD@users.noreply.github.com>
2020-07-25 22:12:11 -0700

    Update UbuntuCloud9Prepare.sh

--- installScripts/UbuntuCloud9Prepare.sh
+++ installScripts/UbuntuCloud9Prepare.sh
@@ -1,7 +1,7 @@
 #source <(curl -s https://raw.githubusercontent.com/johndeebdd/Remote-BDD-Setup/master/installScripts/UbuntuCloud9Prepare.sh)
 curl -sL https://deb.nodesource.com/setup_10.x | sudo -E bash -
-sudo apt install nodejs
+sudo apt install -y nodejs
 sudo apt-get upgrade && sudo apt-get update
 curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.0/install.sh | bash
-sudo apt-get install python-minimal
+sudo apt-get install -y python-minimal
 sudo mkdir ~/cloud9
