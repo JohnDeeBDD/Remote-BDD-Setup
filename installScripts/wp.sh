f0a33acd4d22827929dbe5dd639fd3613b01d846
John Dee <33011694+JohnDeeBDD@users.noreply.github.com>
2020-12-11 11:36:57 -0800

    Update wp.sh

--- installScripts/wp.sh
+++ installScripts/wp.sh
@@ -30,7 +30,7 @@ sudo apt -y install software-properties-common
 sudo add-apt-repository ppa:ondrej/php -y
 sudo apt-get -y update
 sudo apt-get -y install php7.4-common php7.4-mysql php7.4-xml php7.4-xmlrpc php7.4-curl php7.4-gd php7.4-imagick php7.4-cli php7.4-dev php7.4-imap php7.4-mbstring php7.4-opcache php7.4-soap php7.4-zip php7.4-intl
-sudo apt-get -y install php-hash php-json php-sodium php-openssl
+#sudo apt-get -y install php-hash php-json php-sodium php-openssl
 
 sudo apt-get -y install build-essential checkinstall && apt-get build-dep imagemagick -y
 sudo apt-get -y install imagemagick
@@ -45,9 +45,13 @@ sudo apt-get -y install git zip unzip php-zip
 sudo apt-get -y install expect
 sudo debconf-set-selections <<< 'mysql-server mysql-server/root_password password password'
 sudo debconf-set-selections <<< 'mysql-server mysql-server/root_password_again password password'
-sudo apt-get -y install lamp-server^
-sudo a2enmod rewrite
+#sudo apt-get -y install lamp-server^
+#sudo a2enmod rewrite
 sudo apt-get -y install php-mbstring
+
+sudo apt -y install apache2
+sudo ufw allow in "Apache Full"
+sudo apt -y install mysql-server
 
 #setup swap on Ubuntu:
 #sudo /bin/dd if=/dev/zero of=/var/swap.1 bs=1M count=1024
