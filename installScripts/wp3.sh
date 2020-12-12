72ab51e6fabe0041f640ef2e8d2a2e4bd7f78f73
John Dee <33011694+JohnDeeBDD@users.noreply.github.com>
2020-12-12 14:31:52 -0800

    Update wp3.sh

--- installScripts/wp3.sh
+++ installScripts/wp3.sh
@@ -17,31 +17,28 @@ FLUSH PRIVILEGES;
 EOF
 
 # Wordpress plugins:
-#cd /var/www/html/wp-content/plugins
-#sudo rm -fr akismet
-#sudo rm hello.php
-#sudo git clone https://github.com/Hitman007/CRG-Mods.git
-#sudo git clone https://github.com/JohnDeeBDD/WPbdd.git
-#sudo git clone https://github.com/JohnDeeBDD/FastRegister.git
-#sudo git clone https://github.com/JohnDeeBDD/better-error-messages.git
-#sudo git clone https://github.com/JohnDeeBDD/external-content-portfolio.git
-#sudo chmod -R 777 /var/www
-#cd /var/www/html/wp-content/plugins/WPbdd/tests
-#sudo replace "replaceme.com" $varurl -- runner.suite.yml
-#sudo replace "replaceme.com" $varurl -- acceptance.suite.yml
+cd /var/www/html/wp-content/plugins
+sudo rm -fr akismet
+sudo rm hello.php
+sudo git clone https://github.com/Hitman007/CRG-Mods.git
+sudo git clone https://github.com/JohnDeeBDD/WPbdd.git
+sudo git clone https://github.com/JohnDeeBDD/FastRegister.git
+sudo git clone https://github.com/JohnDeeBDD/better-error-messages.git
+sudo git clone https://github.com/JohnDeeBDD/external-content-portfolio.git
+sudo chmod -R 777 /var/www
+cd /var/www/html/wp-content/plugins/WPbdd/tests
+sudo replace "replaceme.com" $varurl -- runner.suite.yml
+sudo replace "replaceme.com" $varurl -- acceptance.suite.yml
 
-#cd /var/www/html/wp-content/plugins/external-content-portfolio/tests
-#sudo replace "replaceme.com" $varurl -- runner.suite.yml
-#sudo replace "replaceme.com" $varurl -- acceptance.suite.yml
-#cd ..
-#composer install
-#cd /var/www/html/wp-content/plugins/WPbdd
-#composer install
+cd /var/www/html/wp-content/plugins/WPbdd
+composer install
+sudo replace "replaceme.com" $varurl -- runner.suite.yml
+sudo replace "replaceme.com" $varurl -- acceptance.suite.yml
 
 #setup apache conf
-#sudo mv /var/www/html/wp-content/plugins/WPbdd/apacheconf.txt /etc/apache2/sites-available/000-default.conf
-#cd /etc/apache2/sites-available/
-#sudo replace replaceme.com $varurl -- 000-default.conf
+sudo mv /var/www/html/wp-content/plugins/WPbdd/apacheconf.txt /etc/apache2/sites-available/000-default.conf
+cd /etc/apache2/sites-available/
+sudo replace replaceme.com $varurl -- 000-default.conf
 
 #install phantomJS
 #sudo apt-get -y install build-essential chrpath libssl-dev libxft-dev
@@ -53,7 +50,7 @@ EOF
 #xvfb-run java -Dwebdriver.chrome.driver=/var/www/html/wp-content/plugins/WPbdd/chromedriver -jar selenium.jar
 
 #install nodeJS
-#sudo apt-get -y install nodejs
+sudo apt-get -y install nodejs
 
 #cleanup:
 #sudo chmod 777 -R /var/www/html
@@ -61,21 +58,21 @@ EOF
 #sudo service apache2 restart
 
 #activate WordPress via the Codeception runner and CLI:
-#cd /var/www/html/wp-content/plugins/WPbdd
-#nohup xvfb-run java -Dwebdriver.chrome.driver=/var/www/html/wp-content/plugins/WPbdd/chromedriver -jar selenium.jar &>/dev/null &
-#bin/codecept build
-#bin/codecept run runner -vvv --html
-#bin/wp theme install responsive-kubrick --activate
-#bin/wp plugin activate FastRegister
-#bin/wp widget add my_widget sidebar-1 1
-#bin/wp rewrite structure '/%postname%/'
+cd /var/www/html/wp-content/plugins/WPbdd
+nohup xvfb-run java -Dwebdriver.chrome.driver=/var/www/html/wp-content/plugins/WPbdd/chromedriver -jar selenium.jar &>/dev/null &
+bin/codecept build
+bin/codecept run runner -vvv --html
+bin/wp theme install responsive-kubrick --activate
+bin/wp plugin activate FastRegister
+bin/wp widget add my_widget sidebar-1 1
+bin/wp rewrite structure '/%postname%/'
 
 #removes password auth
-# sudo sed -i -e '/^PasswordAuthentication / s/ .*/ yes/' /etc/ssh/sshd_config
-# sudo adduser freelancer --gecos "First Last,RoomNumber,WorkPhone,HomePhone" --disabled-password
-# This will actually set the password:
-# echo "freelancer:password" | sudo chpasswd
-# sudo usermod -aG sudo freelancer
+sudo sed -i -e '/^PasswordAuthentication / s/ .*/ yes/' /etc/ssh/sshd_config
+sudo adduser freelancer --gecos "First Last,RoomNumber,WorkPhone,HomePhone" --disabled-password
+#This will actually set the password:
+echo "freelancer:password" | sudo chpasswd
+sudo usermod -aG sudo freelancer
 
 #cloud9
 #curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.0/install.sh | bash
