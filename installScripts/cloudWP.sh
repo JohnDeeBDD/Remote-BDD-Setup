db1a2f09606d9272a44355a043d29070b4529988
John Dee <33011694+JohnDeeBDD@users.noreply.github.com>
2020-12-12 09:40:50 -0800

    Create cloudWP.sh

---
+++ installScripts/cloudWP.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+#source <(curl -s https://raw.githubusercontent.com/johndeebdd/Remote-BDD-Setup/master/installScripts/wcloudWP.sh)
+source <(curl -s https://raw.githubusercontent.com/johndeebdd/Remote-BDD-Setup/master/installScripts/wp1.sh)
+source <(curl -s https://raw.githubusercontent.com/johndeebdd/Remote-BDD-Setup/master/installScripts/wp2.sh)
+source <(curl -s https://raw.githubusercontent.com/johndeebdd/Remote-BDD-Setup/master/installScripts/wp3.sh)
