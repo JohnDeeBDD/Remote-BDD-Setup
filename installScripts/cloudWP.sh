7261c5c9c4b22aa715dd93bbd73478c9187a0834
John Dee <33011694+JohnDeeBDD@users.noreply.github.com>
2020-12-12 14:11:53 -0800

    Update cloudWP.sh

--- installScripts/cloudWP.sh
+++ installScripts/cloudWP.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-#source <(curl -s https://raw.githubusercontent.com/johndeebdd/Remote-BDD-Setup/master/installScripts/wcloudWP.sh)
+#source <(curl -s https://raw.githubusercontent.com/johndeebdd/Remote-BDD-Setup/master/installScripts/cloudWP.sh)
 source <(curl -s https://raw.githubusercontent.com/johndeebdd/Remote-BDD-Setup/master/installScripts/wp1.sh)
 source <(curl -s https://raw.githubusercontent.com/johndeebdd/Remote-BDD-Setup/master/installScripts/wp2.sh)
 source <(curl -s https://raw.githubusercontent.com/johndeebdd/Remote-BDD-Setup/master/installScripts/wp3.sh)
