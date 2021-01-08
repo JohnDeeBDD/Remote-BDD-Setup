7a2b05b6d00e98a415963251af4ec103bf01c71b
John Dee <33011694+JohnDeeBDD@users.noreply.github.com>
2021-01-08 14:36:46 -0800

    Update UbuntuCloud9Prepare.sh

--- installScripts/UbuntuCloud9Prepare.sh
+++ installScripts/UbuntuCloud9Prepare.sh
@@ -4,5 +4,5 @@ sudo apt install -y nodejs
 sudo apt-get -y upgrade
 sudo apt-get -y update
 curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.0/install.sh | bash
-sudo apt-get install -y python-minimal
+sudo apt-get install -y python2-minimal
 sudo mkdir ~/cloud9
