c6379c0dfc715c14b9622ec2b9fd04d1f334a36d
John Dee <33011694+JohnDeeBDD@users.noreply.github.com>
2021-03-01 07:42:18 -0800

    Update ResetWPdatabases.sh

--- installScripts/ResetWPdatabases.sh
+++ installScripts/ResetWPdatabases.sh
@@ -8,7 +8,7 @@
 sudo mysql -u root -ppassword << EOF
 DROP DATABASE wordpress;
 CREATE DATABASE wordpress DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci;
-GRANT ALL ON wordpress.* TO 'wordpressuser'@'localhost' IDENTIFIED BY 'password';
+grant all privileges on wordpress.* to wordpressuser@localhost identified by 'password';
 FLUSH PRIVILEGES;
 EOF
 sudo rm /var/www/html/wp-config.php
