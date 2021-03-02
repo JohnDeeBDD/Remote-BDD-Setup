82e7027a377bdcb2c10bc03055f48daff585eb93
John Dee <33011694+JohnDeeBDD@users.noreply.github.com>
2021-03-02 04:53:15 -0800

    Update wordpress.sh

--- installScripts/wordpress.sh
+++ installScripts/wordpress.sh
@@ -124,6 +124,9 @@ wp plugin activate FastRegister
 wp widget add my_widget sidebar-1 1
 wp rewrite structure '/%postname%/'
 
+cd /var/www/html
+sudo sed -i "s/( 'WP_DEBUG', false )/( 'WP_DEBUG', true )/g" "wp-config.php"
+
 #removes password auth
 # sudo sed -i -e '/^PasswordAuthentication / s/ .*/ yes/' /etc/ssh/sshd_config
 # sudo adduser freelancer --gecos "First Last,RoomNumber,WorkPhone,HomePhone" --disabled-password
