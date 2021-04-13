5ee22d9e5f84df532b44cfb713ffc370a5733ac8
John Dee <33011694+JohnDeeBDD@users.noreply.github.com>
2021-04-13 04:41:16 -0700

    Update wordpress.sh

--- installScripts/wordpress.sh
+++ installScripts/wordpress.sh
@@ -12,14 +12,14 @@
 clear
 echo What is the domain name of the site?
 read varurl
-#wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
-#echo 'deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main' | sudo tee /etc/apt/sources.list.d/google-chrome.list
+wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
+echo 'deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main' | sudo tee /etc/apt/sources.list.d/google-chrome.list
+sudo apt-get -y install google-chrome-stable
 wget --no-verbose -O /tmp/chrome.deb https://dl.google.com/linux/chrome/deb/pool/main/g/google-chrome-stable/google-chrome-stable_88.0.4324.182-1_amd64.deb
-sudo apt install -y /tmp/chrome.deb
+sudo apt install -y /tmp/chrome.deb --allow-downgrades
 sudo rm /tmp/chrome.deb
 sudo apt-get -y update
 sudo apt-get -y upgrade 
-sudo apt-get -y install google-chrome-stable
 sudo apt-get -y install net-tools
 #sudo apt-get -f install
 #sudo apt-get -y install nodejs-legacy
