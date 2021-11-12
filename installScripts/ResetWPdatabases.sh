2554f451ccb7086b4f161c63bf2f265f4851aee0
John Dee <33011694+JohnDeeBDD@users.noreply.github.com>
2021-11-12 15:51:20 -0800

    Update ResetWPdatabases.sh

--- installScripts/ResetWPdatabases.sh
+++ installScripts/ResetWPdatabases.sh
@@ -5,6 +5,9 @@
 # dig +short myip.opendns.com @resolver1.opendns.com
 # https://www.php.net/manual/en/function.yaml-parse-file.php
 
+cd /var/www/html/wp-content/plugins/WPbdd
+nohup xvfb-run java -Dwebdriver.chrome.driver=/var/www/html/wp-content/plugins/WPbdd/chromedriver -jar selenium.jar &>/dev/null &
+
 sudo mysql -u root -ppassword << EOF
 DROP DATABASE wordpress;
 CREATE DATABASE wordpress DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci;
