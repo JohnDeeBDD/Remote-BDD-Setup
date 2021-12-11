94443bc4ad7502c3e2281414c6fec2d5e23b9abf
John Dee <33011694+JohnDeeBDD@users.noreply.github.com>
2021-12-10 16:03:49 -0800

    Update ResetWPdatabases.sh

--- installScripts/ResetWPdatabases.sh
+++ installScripts/ResetWPdatabases.sh
@@ -18,3 +18,4 @@ cd /var/www/html/wp-content/plugins/WPbdd/
 bin/codecept run runner SetupWordPressCept.php -vvv
 wp rewrite structure '/%postname%/'
 wp option update uploads_use_yearmonth_folders 0
+wp user create Subcriberman subscriberman@email.com --role=subscriber --user_pass=password --display_name=Subscriberman
