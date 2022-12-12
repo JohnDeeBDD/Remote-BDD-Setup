64cd24dd382c2924f6237dfb508c16e897ad1cf6
John Dee <33011694+JohnDeeBDD@users.noreply.github.com>
2022-12-11 19:34:17 -0800

    Update PHPenabler.sh

--- installScripts/PHPenabler.sh
+++ installScripts/PHPenabler.sh
@@ -4,16 +4,52 @@
 #  - /etc/php/7.2/cli/conf.d/15-xml.ini
 #  - /etc/php/7.2/cli/conf.d/20-calendar.ini
 # source <(curl -s https://raw.githubusercontent.com/JohnDeeBDD/Remote-BDD-Setup/master/installScripts/PHPenabler.sh)
+sudo apt install php-calendar
+sudo apt install php-ctype
+sudo apt install php-curl
+sudo apt install php-exif
+sudo apt install php-ffi
+sudo apt install php-fileinfo
+sudo apt install php-ftp
+sudo apt install php-gd
+sudo apt install php-gettext
+sudo apt install php-iconv
+sudo apt install php-imagick
+sudo apt install php-json
+sudo apt install php-mbstring
+sudo apt install php-mysqli
+sudo apt install php-mysqlnd
+sudo apt install php-opcache
+sudo apt install php-pdo_mysql
+sudo apt install php-phar
+sudo apt install php-posix
+sudo apt install php-readline
+sudo apt install php-shmop
+sudo apt install php-simplexml
+sudo apt install php-sockets
+sudo apt install php-sysvmsg
+sudo apt install php-sysvsem
+sudo apt install php-sysvshm
+sudo apt install php-tokenizer
+sudo apt install php-wddx
+sudo apt install php-zip
+
+sudo phpenmod calendar
 sudo phpenmod ctype
 sudo phpenmod curl
 sudo phpenmod exif
+sudo phpenmod ffi
 sudo phpenmod fileinfo
 sudo phpenmod ftp
 sudo phpenmod gd
 sudo phpenmod gettext
-sudo phpenmod iconv 
+sudo phpenmod iconv
+sudo phpenmod imagick
 sudo phpenmod json
+sudo phpenmod mbstring
 sudo phpenmod mysqli
+sudo phpenmod mysqlnd
+sudo phpenmod opcache
 sudo phpenmod pdo_mysql
 sudo phpenmod phar
 sudo phpenmod posix
@@ -26,6 +62,7 @@ sudo phpenmod sysvsem
 sudo phpenmod sysvshm
 sudo phpenmod tokenizer
 sudo phpenmod wddx
+sudo phpenmod zip
 sudo phpenmod xmlreader
 sudo phpenmod xmlrpc
 sudo phpenmod xmlwriter
