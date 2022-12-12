ca0a7acce3d5ec2afddd6b333fecfa0e163db770
John Dee <33011694+JohnDeeBDD@users.noreply.github.com>
2022-12-11 16:26:10 -0800

    Update UbuntuCloud9Prepare.sh

--- installScripts/UbuntuCloud9Prepare.sh
+++ installScripts/UbuntuCloud9Prepare.sh
@@ -1,6 +1,7 @@
 #source <(curl -s https://raw.githubusercontent.com/johndeebdd/Remote-BDD-Setup/master/installScripts/UbuntuCloud9Prepare.sh)
-curl -sL https://deb.nodesource.com/setup_14.x | sudo -E bash -
-sudo apt install -y nodejs
+#curl -sL https://deb.nodesource.com/setup_14.x | sudo -E bash -
+#sudo apt install -y nodejs
+sudo snap install node --classic
 sudo apt-get -y upgrade
 sudo apt-get -y update
 curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.0/install.sh | bash
