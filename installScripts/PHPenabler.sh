e650b32a781451448631f8f6a428622054a57776
John Dee <33011694+JohnDeeBDD@users.noreply.github.com>
2023-01-25 18:15:53 -0800

    Update PHPenabler.sh

--- installScripts/PHPenabler.sh
+++ installScripts/PHPenabler.sh
@@ -4,35 +4,35 @@
 #  - /etc/php/7.2/cli/conf.d/15-xml.ini
 #  - /etc/php/7.2/cli/conf.d/20-calendar.ini
 # source <(curl -s https://raw.githubusercontent.com/JohnDeeBDD/Remote-BDD-Setup/master/installScripts/PHPenabler.sh)
-sudo apt install php-calendar
-sudo apt install php-ctype
-sudo apt install php-curl
-sudo apt install php-exif
-sudo apt install php-ffi
-sudo apt install php-fileinfo
-sudo apt install php-ftp
-sudo apt install php-gd
-sudo apt install php-gettext
-sudo apt install php-iconv
-sudo apt install php-imagick
-sudo apt install php-json
-sudo apt install php-mbstring
-sudo apt install php-mysqli
-sudo apt install php-mysqlnd
-sudo apt install php-opcache
-sudo apt install php-pdo_mysql
-sudo apt install php-phar
-sudo apt install php-posix
-sudo apt install php-readline
-sudo apt install php-shmop
-sudo apt install php-simplexml
-sudo apt install php-sockets
-sudo apt install php-sysvmsg
-sudo apt install php-sysvsem
-sudo apt install php-sysvshm
-sudo apt install php-tokenizer
-sudo apt install php-wddx
-sudo apt install php-zip
+sudo apt install php7.4-calendar
+sudo apt install php7.4-ctype
+sudo apt install php7.4-curl
+sudo apt install php7.4-exif
+sudo apt install php7.4-ffi
+sudo apt install php7.4-fileinfo
+sudo apt install php7.4-ftp
+sudo apt install php7.4-gd
+sudo apt install php7.4-gettext
+sudo apt install php7.4-iconv
+sudo apt install php7.4-imagick
+sudo apt install php7.4-json
+sudo apt install php7.4-mbstring
+sudo apt install php7.4-mysqli
+sudo apt install php7.4-mysqlnd
+sudo apt install php7.4-opcache
+sudo apt install php7.4-pdo_mysql
+sudo apt install php7.4-phar
+sudo apt install php7.4-posix
+sudo apt install php7.4-readline
+sudo apt install php7.4-shmop
+sudo apt install php7.4-simplexml
+sudo apt install php7.4-sockets
+sudo apt install php7.4-sysvmsg
+sudo apt install php7.4-sysvsem
+sudo apt install php7.4-sysvshm
+sudo apt install php7.4-tokenizer
+sudo apt install php7.4-wddx
+sudo apt install php7.4-zip
 
 sudo phpenmod calendar
 sudo phpenmod ctype
