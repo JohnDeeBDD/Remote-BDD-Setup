8c59d9f2d11da6226f2053601695c104b64b1a99
John Dee <33011694+JohnDeeBDD@users.noreply.github.com>
2023-03-14 14:37:46 -0700

    Update wordpress.sh

--- installScripts/wordpress.sh
+++ installScripts/wordpress.sh
@@ -126,9 +126,9 @@ wp plugin install disable-welcome-messages-and-tips
 sudo git clone https://github.com/JohnDeeBDD/WPbdd.git
 sudo git clone https://github.com/JohnDeeBDD/FastRegister.git
 sudo git clone https://github.com/JohnDeeBDD/better-error-messages.git
-sudo git clone https://github.com/JohnDeeBDD/email-tunnel.git
+#sudo git clone https://github.com/JohnDeeBDD/email-tunnel.git
 sudo chmod -R 777 /var/www
-cd /var/www/html/wp-content/plugins/WPbdd
+#cd /var/www/html/wp-content/plugins/WPbdd
 #composer install
 #cd /var/www/html/wp-content/plugins/WPbdd/tests
 #sudo sed -i "s/replaceme.com/$varurl/g" "runner.suite.yml"
