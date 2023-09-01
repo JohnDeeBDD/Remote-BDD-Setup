ca3269a23c2bb5c782e099dd8967d89ce8646763
John Dee <33011694+JohnDeeBDD@users.noreply.github.com>
2023-09-01 14:26:08 -0700

    Update ResetWPdatabases.sh

--- installScripts/ResetWPdatabases.sh
+++ installScripts/ResetWPdatabases.sh
@@ -12,17 +12,3 @@ CREATE DATABASE wordpress DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci;
 GRANT ALL PRIVILEGES ON * . * TO 'wordpressuser'@'localhost';
 FLUSH PRIVILEGES;
 EOF
-cd /var/www/html
-sudo rm wp-config.php
-cd /var/www/html/wp-content/plugins/WPbdd/
-bin/codecept run acceptance SetupWordPressCept.php -vvv
-wp rewrite structure '/%postname%/'
-wp option update uploads_use_yearmonth_folders 0
-wp user create Subcriberman subscriberman@email.com --role=subscriber --user_pass=password --display_name=Subscriberman
-wp plugin activate better-error-messages
-wp plugin activate fcfs-block
-wp plugin activate disable-administration-email-verification-prompt
-wp plugin activate disable-welcome-messages-and-tips
-wp plugin activate email-log
-wp plugin activate email-tunnel
-wp post delete 1 --force
